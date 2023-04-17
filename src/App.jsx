5c5ab3d315de8d713bad98e8b72f0a0897e20bbe
Martin <office@martinpagels.de>
2023-04-18 00:55:34 +0200

    prettierrc works

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,97 @@
 import { useState } from 'react'
-import {createWorker} from 'tesseract.js';
+import { createWorker } from 'tesseract.js'
 
-import styled from 'styled-components';
+import styled from 'styled-components'
 import './App.css'
-import Modal from './components/Modal';
+import Modal from './components/Modal'
 
 function App() {
-  
-  const [isCalculating, setIsCalculating] = useState(false);
+  const [isCalculating, setIsCalculating] = useState(false)
 
-
-  const [textExtract, setTextExtract] = useState("")
+  const [textExtract, setTextExtract] = useState('')
   const [logger, setLogger] = useState(null)
 
-  const loggerInitialStrings = ["loading tesseract core", "initializing tesseract", "loading language traineddata", "loaded language traineddata", "initializing api", "initialized api"]
-  const [image, setImage] = useState(null);
+  const loggerInitialStrings = [
+    'loading tesseract core',
+    'initializing tesseract',
+    'loading language traineddata',
+    'loaded language traineddata',
+    'initializing api',
+    'initialized api',
+  ]
+  const [image, setImage] = useState(null)
 
   function handleImageInputChange(event) {
-    setImage(URL.createObjectURL(event.target.files[0]));
-    setTextExtract("")
+    setImage(URL.createObjectURL(event.target.files[0]))
+    setTextExtract('')
   }
 
   const handleExtractText = async () => {
     setIsCalculating(true)
 
-   
     const worker = await createWorker({
       workerPath: '/services/tesseract/worker.min.js',
       langPath: '/assets/lang-data',
       corePath: '/services/tesseract-js-core/tesseract-core.wasm.js',
-      logger: m => {console.log(m); setLogger(m)},
-    });
+      logger: (m) => {
+        console.log(m)
+        setLogger(m)
+      },
+    })
 
-    (async () => {
-      await worker.load();
-      await worker.loadLanguage('deu');
-      await worker.initialize('deu');
-      const { data: { text } } = await worker.recognize(image);
-      setTextExtract(text);
+    ;(async () => {
+      await worker.load()
+      await worker.loadLanguage('deu')
+      await worker.initialize('deu')
+      const {
+        data: { text },
+      } = await worker.recognize(image)
+      setTextExtract(text)
       setIsCalculating(false)
-      console.log(text);
-      await worker.terminate();
-    })();
-  };
-   
+      console.log(text)
+      await worker.terminate()
+    })()
+  }
 
   return (
-    
-
     <div className="App">
-       <Modal isOpen={isCalculating} title="Extracting Text" message={logger && loggerInitialStrings.some(string => string === logger.status) ? "Prepare scanning..." : logger && logger.progress < 1 && logger.status === "recognizing text" ? `Scanning text... ${(logger.progress * 100).toFixed(0) }%` : "  "} />
-    <ImageInputContainer>
-      <ImageInputTitle>Upload an Image that contains (german) text</ImageInputTitle>
-      <ButtonWrapper>
-
-      <ImageInputLabel htmlFor="image-upload">
-        Choose an Image
-        <ImageInput type="file" id="image-upload" onChange={handleImageInputChange} />
-      </ImageInputLabel>
-      {image && <CalculateButton onClick={handleExtractText}>Extract Text</CalculateButton>}
-      </ButtonWrapper>
-       {textExtract && <pre>
-      {textExtract}</pre>}
-      {image && <ImageInputPreview src={image} alt="User uploaded" />}
-    </ImageInputContainer>
- 
-
-   
-  
-   </div>
-   
-
+      <Modal
+        isOpen={isCalculating}
+        title="Extracting Text"
+        message={
+          logger &&
+          loggerInitialStrings.some((string) => string === logger.status)
+            ? 'Prepare scanning...'
+            : logger &&
+              logger.progress < 1 &&
+              logger.status === 'recognizing text'
+            ? `Scanning text... ${(logger.progress * 100).toFixed(0)}%`
+            : '  '
+        }
+      />
+      <ImageInputContainer>
+        <ImageInputTitle>
+          Upload an Image that contains (german) text
+        </ImageInputTitle>
+        <ButtonWrapper>
+          <ImageInputLabel htmlFor="image-upload">
+            Choose an Image
+            <ImageInput
+              type="file"
+              id="image-upload"
+              onChange={handleImageInputChange}
+            />
+          </ImageInputLabel>
+          {image && (
+            <CalculateButton onClick={handleExtractText}>
+              Extract Text
+            </CalculateButton>
+          )}
+        </ButtonWrapper>
+        {textExtract && <pre>{textExtract}</pre>}
+        {image && <ImageInputPreview src={image} alt="User uploaded" />}
+      </ImageInputContainer>
+    </div>
   )
 }
 
@@ -78,11 +99,10 @@ export default App
 
 const ButtonWrapper = styled.div`
   display: flex;
-
 `
 
 const CalculateButton = styled.button`
- border-radius: 8px;
+  border-radius: 8px;
   padding: 12px 24px;
   background-color: #000;
   color: #fff;
@@ -105,7 +125,7 @@ const ImageInputContainer = styled.div`
   align-items: center;
   margin: 0 auto;
   max-width: 600px;
-`;
+`
 
 const ImageInputTitle = styled.h2`
   font-size: 40px;
@@ -113,12 +133,12 @@ const ImageInputTitle = styled.h2`
   text-align: center;
   margin: 0;
   margin-bottom: 24px;
-`;
+`
 
 const ImageInputPreview = styled.img`
   max-width: 100%;
   margin-top: 24px;
-`;
+`
 
 const ImageInputLabel = styled.label`
   border-radius: 8px;
@@ -136,8 +156,8 @@ const ImageInputLabel = styled.label`
     transform: translateY(-2px);
     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
   }
-`;
+`
 
 const ImageInput = styled.input`
   display: none;
-`;
+`
